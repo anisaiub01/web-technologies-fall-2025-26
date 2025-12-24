ba33d368854b71855ca8c4bacc715d7300187784
anisaiub01 <133850089+anisaiub01@users.noreply.github.com>
2025-12-25 01:05:52 +0600

    Add files via upload

---
+++ lab exam/index.js
@@ -0,0 +1,21 @@
+const display = docment.getElementById("display");
+function appendToDisplay(input)
+{
+    display.value += input;
+    
+}
+function clearDisplay()
+{
+    display.value = " ";
+
+}
+function calculate()
+{
+   try{
+    display.value = eval(display.value);
+   }
+   catch(error)
+   {
+    display.value = " error";
+   }
+}
